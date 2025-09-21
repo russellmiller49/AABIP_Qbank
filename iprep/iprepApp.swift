24ae1781a36f1f8c7fe2d2d925adb25d58378f9a
Russellmiller <russellmiller49@gmail.com>
2025-09-20 19:15:42 -0700

    Initial Commit

---
+++ iprep/iprepApp.swift
@@ -0,0 +1,17 @@
+//
+//  iprepApp.swift
+//  iprep
+//
+//  Created by Russell Miller on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct iprepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
